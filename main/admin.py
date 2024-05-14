b060296de60ef790c36e8581c2f12db1a8d88e0f
Diamond Hands <diamondhands.space@gmail.com>
2024-05-14 10:59:38 +0545

    created migration file,  migrated to test db:sqlite3 and registered model to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stock, StockPrice
+
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ["symbol", "exchange", "company_name"]
+    search_fields = ["symbol", "company_name"]
+
+
+admin.site.register(Stock, StockAdmin)
+
+
+class StockPriceAdmin(admin.ModelAdmin):
+    list_display = ["stock", "date", "open", "high", "low", "close", "volume"]
+    search_fields = ["stock__symbol", "date"]
+    list_filter = ["date", "stock"]
+
+
+admin.site.register(StockPrice, StockPriceAdmin)
