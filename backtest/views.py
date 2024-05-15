1eba379da8422fd805d183f563243b55313465dc
Diamond Hands <diamondhands.space@gmail.com>
2024-05-15 17:08:48 +0545

    created templates for forex and crypto

--- backtest/views.py
+++ backtest/views.py
@@ -8,10 +8,6 @@ def home(request):
     return render(request, "backtest/home.html")
 
 
-def forex(request):
-    return HttpResponse("Forex Page")
-
-
 def stock(request):
     if request.method == "POST":
         form = StockForm(request.POST)
@@ -23,4 +19,20 @@ def stock(request):
 
 
 def crypto(request):
-    return render(request, "backtest/crypto.html")
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StockForm()
+    return render(request, "backtest/crypto.html", {"form": form})
+
+
+def forex(request):
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StockForm()
+    return render(request, "backtest/forex.html", {"form": form})
