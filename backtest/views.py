89d51427188e0fc4974a6a8a083f765714441a9a
Diamond Hands <diamondhands.space@gmail.com>
2024-05-20 08:38:17 +0545

    migrated crypto tables

--- backtest/views.py
+++ backtest/views.py
@@ -11,7 +11,7 @@ from .forms import (
     FutureTestCryptoForm,
     FutureTestForm,
 )
-from .models import Stock
+from .models import Crypto, Stock
 
 
 def home(request):
@@ -109,7 +109,7 @@ def crypto(request):
         if "f_test" in request.POST:
             f_form = FutureTestCryptoForm(request.POST)
         if f_form.is_valid():
-            f_company = Stock.objects.get(symbol=f_form.cleaned_data["f_crypto"])
+            f_crypto = Crypto.objects.get(symbol=f_form.cleaned_data["f_crypto"])
             start_date = f_form.cleaned_data["f_start_date"]
             end_date = f_form.cleaned_data["f_end_date"]
             amount = f_form.cleaned_data["f_amount"]
@@ -119,7 +119,7 @@ def crypto(request):
             context.update(
                 {
                     "f_success": True,
-                    "f_crypto": f_company.company_name,
+                    "f_crypto": f_crypto.crypto_name,
                     "f_start_date": start_date,
                     "f_end_date": end_date,
                     "f_starting_amount": amount,
@@ -154,33 +154,41 @@ def crypto(request):
             )
 
         if "b_test" in request.POST:
-            b_form = BackTestForm(request.POST)
+            b_form = BackTestCryptoForm(request.POST)
             if b_form.is_valid():
-                b_calc = Calculator(
-                    b_form.cleaned_data["b_start_date"],
-                    b_form.cleaned_data["b_end_date"],
-                    b_form.cleaned_data["b_crypto"],
-                    b_form.cleaned_data["b_amount"],
-                    None,
-                )
-                b_company = Stock.objects.get(symbol=b_form.cleaned_data["b_crypto"])
+                b_crypto = Crypto.objects.get(symbol=b_form.cleaned_data["b_crypto"])
+                start_date = b_form.cleaned_data["b_start_date"]
+                end_date = b_form.cleaned_data["b_end_date"]
+                amount = b_form.cleaned_data["b_amount"]
+                symbol = b_form.cleaned_data["b_crypto"]
+
                 context.update(
                     {
                         "b_success": True,
-                        "b_crypto": b_company.company_name,
-                        "b_start_date": b_form.cleaned_data["b_start_date"],
-                        "b_end_date": b_form.cleaned_data["b_end_date"],
-                        "b_starting_amount": b_form.cleaned_data["b_amount"],
-                        "b_ending_amount": b_calc.ending_amount(),
-                        "b_start_opening_price": b_calc.start_date_opening_price(),
-                        "b_end_opening_price": b_calc.end_date_opening_price(),
-                        "b_total_shares": b_calc.total_shares(),
-                        "b_difference_in_amount": b_calc.difference_in_amount(),
-                        "b_difference_in_percentage": b_calc.difference_in_percentage(),
-                        "b_yearly_return": b_calc.year_to_year_return(),
-                        "b_monthly_return": b_calc.month_to_month_return(),
-                        "b_state": b_calc.state(),
-                        "b_period_values": b_calc.get_periods_values(),
+                        "b_crypto": b_crypto.crypto_name,
+                        "b_start_date": start_date,
+                        "b_end_date": end_date,
+                        "b_starting_amount": amount,
+                        "b_ending_amount": ending_amount(end_date, amount, symbol),
+                        "b_start_opening_price": day_opening_price(start_date, symbol),
+                        "b_end_opening_price": day_opening_price(end_date, symbol),
+                        "b_total_shares": total_shares(start_date, amount, symbol),
+                        "b_difference_in_amount": difference_in_amount(
+                            start_date, end_date, amount, symbol
+                        ),
+                        "b_difference_in_percentage": difference_in_percentage(
+                            start_date, end_date, amount, symbol
+                        ),
+                        "b_yearly_return": year_to_year_return(
+                            start_date, end_date, amount, symbol
+                        ),
+                        "b_monthly_return": month_to_month_return(
+                            start_date, end_date, amount, symbol
+                        ),
+                        "b_state": state(start_date, end_date, amount, symbol),
+                        "b_period_values": get_periods_values(
+                            start_date, end_date, amount, symbol
+                        ),
                         "b_form": b_form,
                         "tab_id": "one",
                         "link_id": "one_link",
@@ -191,3 +199,7 @@ def crypto(request):
     context.update({"f_form": f_form, "b_form": b_form})
     pprint(context)
     return render(request, "backtest/crypto.html", context=context)
+
+
+def forex(request):
+    return HttpResponse("Hi")
